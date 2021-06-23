58d01c10d4dcbaeadd9bdffbaa8acfb0e48bfdf9
cristian <cnovoa.o@gmail.com>
2021-06-23 14:13:54 +0300

    2

--- PreviouslyOnJS.js
+++ PreviouslyOnJS.js
@@ -22,3 +22,26 @@ var removedFromMyArray = myArray.pop();   // removes [-1]
 var myArray = [["John", 23], ["dog", 3]];
 myArray.unshift(["Paul", 35])             // adds element at the beginning
 myArray.push(["Paul", 35])                // adds element at the end
+
+function myLocalScope() {
+  var myVar = 'local';
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();                             // outputs 'inside myLocalScope local'
+console.log('outside myLocalScope', myVar); // outputs 'myVar undefined'
+
+
+var outerWear = "T-Shirt";
+function myOutfit() {
+  var outerWear = 'sweater'
+  return outerWear;
+}
+myOutfit();                   // outputs 'sweater', local variable overrides global
+
+
+var sum = 0;
+function addFive () {
+  sum = sum + 5;
+}
+addFive();                    // outputs 'undefined' cause lack of return statement
+                              // it changes the global var sum
