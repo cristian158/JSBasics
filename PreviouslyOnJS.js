68d1eed3c1c5d4d6b05553a59c1947c98772db8d
cristian <cnovoa.o@gmail.com>
2021-07-27 02:01:09 +0200

    added more stuff

--- PreviouslyOnJS.js
+++ PreviouslyOnJS.js
@@ -57,3 +57,120 @@ var testArr = [1,2,3,4,5];
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
+
+
+
+function testEqual(val) {
+  if (val == 12) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
+
+
+function testLogicalAnd(val) {
+  if (val<=50 && val>=25) {       // yes if lessequal 50 or greaterequal 25
+      return "Yes";
+  }
+  return "No";
+}
+testLogicalAnd(10);
+
+
+function testLogicalOr(val) {
+  if (val<10 || val>20) {
+    return "Outside";
+  }
+  return "Inside";
+}
+testLogicalOr(15);
+
+
+function testElse(val) {
+  var result = "";
+  if (val > 5) {
+    result = "Bigger than 5";
+  }  else {
+    result = "5 or Smaller";
+  }
+  return result;
+}
+testElse(4);
+
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+  else if (val < 5) {
+    return "Smaller than 5";
+  }
+  else {
+  return "Between 5 and 10";
+  }
+}
+testElseIf(7);
+// OR
+function testElseIf(val) {
+  if (val > 10) {return "Greater than 10";}
+  else if (val < 5) {return "Smaller than 5";}
+  else {return "Between 5 and 10";}
+}
+testElseIf(7);						// returns 'Between 5 and 10'
+
+
+function orderMyLogic(val) {		// order of statements, matter
+  if (val < 5) {return "Less than 5";}
+  else if (val < 10) {return "Less than 10";}
+  else {return "Greater than or equal to 10";}
+}
+orderMyLogic(7);					//returns 'Less than 10'
+
+
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  if (strokes == 1) {
+    return names[0];
+  } else if (strokes <= (par -2)) {
+    return names[1];
+  } else if (strokes == (par - 1)) {
+    return names[2];
+  } else if (par == strokes) {
+    return names[3];
+  } else if (strokes == (par + 1)) {
+    return names[4];
+  } else if (strokes == (par + 2)) {
+    return names[5];
+  } else if (strokes >= (par + 3)) {
+    return names[6];
+  }
+  return "Change Me";
+}
+golfScore(5, 4);					// returns 'Birdie'
+
+
+function caseInSwitch(val) {
+  var answer = "";
+  switch(val) {
+    case 1:
+      answer = 'alpha';
+      break;
+    case 2:
+      answer = 'beta';
+      break;
+    case 3:
+      answer = 'gamma';
+      break;
+	case 4:
+	case 5:
+		answer = 'etc'
+    default:
+      answer = 'another letter';
+      break;
+  }
+  return answer;
+}
+caseInSwitch(1);					// returns 'alpha' after inputting '1'
+									// 4 & 5 returns 'etc'
